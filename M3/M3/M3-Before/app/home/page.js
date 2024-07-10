9ef5e5018a2d8b37f50a5acdafc5ba9480c306ad
Fabio Benavides <Fabio.Benavides@Henryscheinone.com>
2024-07-10 17:01:52 -0600

    Adding some links

--- M3/M3/M3-Before/app/home/page.js
+++ M3/M3/M3-Before/app/home/page.js
@@ -1,7 +1,18 @@
+import Link from "next/link";
+
 export default function Page() {
     return (
       <>
         <h1>Welcome to Home App</h1>
+        <h2>
+          <Link href="/blog">blog</Link>
+        </h2>
+        <h2>
+          <Link href="/conference">conference</Link>
+        </h2>
+        <h2>
+          <Link href="/settings">Settings</Link>
+        </h2>
       </>
     );
   }
