fa3dd2966a4a7d9a7ce3a41264fb015dbe666609
Fabio Benavides <Fabio.Benavides@Henryscheinone.com>
2024-07-15 16:52:06 -0600

    Module 4 changes

--- M4-Before/app/layout.js
+++ M4-Before/app/layout.js
@@ -1,6 +1,13 @@
+/*import { Open_Sans } from "@next/font/google";
+
+const openSans = Open_Sans({
+  subsets: ["lating"],
+  weight: ["400"],
+});
+*/
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" /*className={openSans.className}*/>
       <head />
       <body>{children}</body>
     </html>
